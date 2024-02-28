f5a0ebbd70213a54c7fe108b000bb9c72b02e148
Vitaliy <vitalikrud03@example.com>
2024-02-29 00:20:51 +0300

    env: set CI, eslint, prettier, husky, storybook, jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  preset: 'ts-jest',
+};
+
+module.exports = createJestConfig(config);
